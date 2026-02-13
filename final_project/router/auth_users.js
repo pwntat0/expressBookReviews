ea77968e04ddd274a4315bf123fbcd1405fc1dd6
Josh Askin <potatojoshy@gmail.com>
2026-02-13 09:28:53 -0500

    Task 6-9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,23 +6,68 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        return res.status(404).json({ message: "Please submit both username and password." });
+    }
+
+    if (authenticatedUser(username, password)) {
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password." });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const name = req.session.authorization.username;
+    let reviews = books[isbn].reviews;
+
+    reviews.name = req.body.review;
+    return res.status(200).json({ message: "Your review has been successfully submitted." });
+
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const name = req.session.authorization.username;
+    let reviews = books[isbn].reviews;
+
+    delete reviews.name;
+    return res.status(200).json({ message: "Your review has been successfully deleted." });
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,36 +6,46 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    res.send(req.body)
+    // if (username && password) {
+    //     if (!isValid(username)) {
+    //         users.push({"username": username, "password": password});
+    //         return res.status(200).json({message: "User successfully registered. Now you can login"});
+    //     } else {
+    //         return res.status(404).json({message: "User already exists!"});
+    //     }
+    // }
+    // return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books,null,4));
+    res.send(JSON.stringify(users,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  if (books[isbn]) {
-    res.send(books[isbn]);
-  } else {
-    return res.status(404).json({message: `The book with ISBN: ${isbn} does not exist.`});
-  }
- });
-  
+    const isbn = req.params.isbn;
+    if (books[isbn]) {
+        res.send(books[isbn]);
+    } else {
+        return res.status(404).json({message: `The book with ISBN: ${isbn} does not exist.`});
+    }
+});
+    
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  let book_array = Object.entries(books);
-  let filtered_books = book_array.filter((book) => book[1].author === author);
-  let book_object = Object.fromEntries(filtered_books);
-  if (Object.keys(book_object).length !== 0) {
-    res.send(book_object);
-  } else {
-    return res.status(404).json({message: `There are no books under the author: ${author}.`});
-  }
+    const author = req.params.author;
+    let book_array = Object.entries(books);
+    let filtered_books = book_array.filter((book) => book[1].author === author);
+    let book_object = Object.fromEntries(filtered_books);
+    if (Object.keys(book_object).length !== 0) {
+        res.send(book_object);
+    } else {
+        return res.status(404).json({message: `There are no books under the author: ${author}.`});
+    }
 });
 
 // Get all books based on title
@@ -45,21 +55,21 @@ public_users.get('/title/:title',function (req, res) {
     let filtered_books = book_array.filter((book) => book[1].title === title);
     let book_object = Object.fromEntries(filtered_books);
     if (Object.keys(book_object).length !== 0) {
-      res.send(book_object);
+        res.send(book_object);
     } else {
-      return res.status(404).json({message: `There are no books under the title: ${title}.`});
+        return res.status(404).json({message: `There are no books under the title: ${title}.`});
     }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  res.send(books[isbn].reviews);
-//   if (Object.keys(books[isbn].reviews).length !== 0) {
-//     res.send(books[isbn].reviews);
-//   } else {
-//     return res.status(404).json({message: `There are no reviews under the book with ISBN: ${isbn}.`});
-//   }
+    const isbn = req.params.isbn;
+    res.send(books[isbn].reviews);
+    //   if (Object.keys(books[isbn].reviews).length !== 0) {
+    //     res.send(books[isbn].reviews);
+    //   } else {
+    //     return res.status(404).json({message: `There are no reviews under the book with ISBN: ${isbn}.`});
+    //   }
 });
 
 module.exports.general = public_users;
